5543e490b734f56447833d820a70f5354050de1d
lsegal <lsegal@users.noreply.github.com>
2020-01-03 02:21:02 +0000

    Deploy to GitHub pages

---
+++ js/global.js
@@ -0,0 +1,115 @@
+function generateTOC() {
+  var _toc = $('<ol class="top"></ol>');
+  var show = false;
+  var toc = _toc;
+  var counter = 0;
+  var tags = ['h2', 'h3', 'h4', 'h5', 'h6'];
+  if ($('#content h1').length > 1) tags.unshift('h1');
+  for (i in tags) { tags[i] = '#content ' + tags[i] }
+  var lastTag = parseInt(tags[0][1]);
+  $(tags.join(', ')).each(function() {
+    if ($(this).hasClass('entry')) return;
+    show = true;
+    var thisTag = parseInt(this.tagName[1]);
+    if (this.id.length == 0) {
+      var proposedId = $(this).text().replace(/[^a-z]/ig, '_');
+      if ($('#' + proposedId).length > 0) proposedId += counter++;
+      this.id = proposedId;
+    }
+    if (thisTag > lastTag) { 
+      for (var i = 0; i < thisTag - lastTag; i++) { 
+        var tmp = $('<ol/>'); toc.append(tmp); toc = tmp; 
+      } 
+    }
+    if (thisTag < lastTag) { 
+      for (var i = 0; i < lastTag - thisTag; i++) toc = toc.parent(); 
+    }
+    toc.append('<li><a href="#' + this.id + '">' + $(this).text() + '</a></li>');
+    lastTag = thisTag;
+  });
+  if (!show) return;
+  html = '<div id="toc" class="nofloat"><p class="title"><a class="hide_toc" href="#"><strong>Table of Contents</strong></a> <small>(<a href="#" class="float_toc">float</a>)</small></p></div>';
+  $('#content h1').after(html);
+  $('#toc').append(_toc);
+  $('#toc .hide_toc').toggle(function() { 
+    $('#toc .top').slideUp('fast');
+    $('#toc').toggleClass('hidden');
+    $('#toc .title small').toggle();
+  }, function() {
+    $('#toc .top').slideDown('fast');
+    $('#toc').toggleClass('hidden');
+    $('#toc .title small').toggle();
+  });
+  $('#toc .float_toc').toggle(function() { 
+    $(this).text('left');
+    $('#toc').toggleClass('nofloat');
+  }, function() {
+    $(this).text('float')
+    $('#toc').toggleClass('nofloat');
+  });
+}
+
+// hero
+document.addEventListener('DOMContentLoaded', () => {
+  const hero = document.getElementById('hero');
+  if (!hero) return;
+
+  const showcase = document.getElementById('showcase');
+  const blurbs = hero.querySelectorAll('.blurb');
+  const selector = hero.querySelector('.selector');
+
+  function advanceTile(n) {
+    if (!n) n = 1;
+    var next = parseInt(hero.dataset.selected) + n;
+    if (next > blurbs.length) next = 1;
+    if (next <= 0) next = blurbs.length;
+    hero.dataset.selected = next.toString();
+  }
+
+  var advanceInterval = null;
+  function setAdvanceInterval() {
+    if (advanceInterval) clearInterval(advanceInterval);
+    advanceInterval = setInterval(advanceTile, 8000);
+  }
+
+  blurbs.forEach((el, idx) => {
+    const num = idx + 1;
+    const hdr = document.createElement('h1');
+    const div = document.createElement('div');
+    hdr.innerText = el.querySelector('h2').innerText;
+    div.classList.add(`tile${num}`);
+    el.classList.add(`tile${num}`);
+    div.style.backgroundImage = `url('${el.dataset.bg}')`;
+    div.appendChild(hdr);
+    showcase.appendChild(div);
+
+    const sel = document.createElement('i');
+    sel.className = 'fas fa-circle';
+    sel.dataset.selection = num;
+    selector.appendChild(sel);
+
+    el.addEventListener('mouseout', () => {
+      if (!advanceInterval) setAdvanceInterval();
+    });
+    el.addEventListener('click', () => {
+      hero.dataset.selected = num.toString();
+      clearInterval(advanceInterval);
+      advanceInterval = null;
+    });
+
+    sel.addEventListener('click', () => {
+      hero.dataset.selected = num.toString();
+      setAdvanceInterval();
+    });
+  });
+
+  setAdvanceInterval();
+  hero.querySelector('.fa-angle-left').addEventListener('click', () => {
+    setAdvanceInterval();
+    advanceTile(-1);
+  });
+  hero.querySelector('.fa-angle-right').addEventListener('click', () => {
+    setAdvanceInterval();
+    advanceTile();
+  });
+});
---
+++ js/types.js
@@ -0,0 +1,186 @@
+const TOKENS = {
+  collection_start: /^</,
+  collection_end: /^>/,
+  fixed_collection_start: /^\(/,
+  fixed_collection_end: /^\)/,
+  type_name: /^(?:#\w+|((::)?\w+)+)/,
+  type_next: /^[,;]/,
+  whitespace: /^\s+/,
+  hash_collection_start: /^\{/,
+  hash_collection_next: /^=>/,
+  hash_collection_end: /^\}/,
+  literal: /^(?::\w+|(["']).+?\1)/,
+  parse_end: null
+};
+
+const BreakException = {};
+
+class SyntaxError extends Error {}
+
+class Type {
+  toString(plural) {
+    if (this.name[0] === "#") {
+      return plural ? `objects that respond to ${this.name}` : `an object that responds to ${this.name}`;
+    } else if (this.name[0].match(/["':]/)) {
+      return plural ? `literal values of ${this.name}` : `a literal value ${this.name}`;
+    } else if (this.name[0].match(/[A-Z]/)) {
+      return plural ? `${this.name}${this.name[this.name.length - 1].match(/[A-Z]/) ? "'" : ''}s` : 
+        `a${this.name[0].match(/[aeiou]/i) ? 'n' : ''} ` + this.name;
+    }
+    return this.name;
+  }
+}
+
+class CollectionType extends Type {
+  constructor() {
+    super();
+    this.types = [];
+  }
+
+  toString() {
+    return `a${this.name[0].match(/[aeiou]/i) ? 'n' : ''} ${this.name} of (` +
+           this.types.map((t) => t.toString(true)).join(', ') + ")";
+  }
+}
+
+class FixedCollectionType extends CollectionType {
+  toString() {
+    return `a${this.name[0].match(/[aeiou]/i) ? 'n' : ''} ${this.name} containing (` +
+           this.types.map((t) => t.toString()).join(" followed by ") + ")";
+  }
+}
+
+class HashCollectionType extends Type {
+  toString() {
+    return `a${this.name[0].match(/[aeiou]/i) ? 'n' : ''} ${this.name} with keys made of (` +
+           this.key_types.map((t) => t.toString(true)).join(', ') + 
+           ") and values of (" +
+           this.value_types.map((t) => t.toString(true)).join(', ') +
+           ")";
+  }
+}
+
+class TypesParser {
+  constructor(input) {
+    this.input = input;
+    this.pos = 0;
+  }
+
+  static parse(input) {
+    return new TypesParser(input).parse();
+  }
+
+  parse() {
+    var types = [];
+    var type = null;
+    var name = null;
+    var result = null;
+  
+    try {
+      while (result === null) {
+        if (!this.input || this.input.length === 0) {
+          if (!name) {
+            throw new SyntaxError('expecting name, got EOF');
+          }
+          if (!type) {
+            type = new Type;
+            type.name = name;
+          }
+          types.push(type);
+          result = types;
+          break;
+        }
+  
+        try {
+          Object.keys(TOKENS).forEach((key) => {
+            const val = TOKENS[key];
+            const match = this.input.match(val);
+            if (match === null) return;
+    
+            // advance input
+            const token = match[0];
+            this.pos += token.length
+            this.input = this.input.substr(token.length);
+
+            // handle token
+            if (key === 'type_name' || key === 'literal') {
+              if (name) {
+                throw new SyntaxError(`expecting END, got name '${token}'`);
+              }
+              name = token;
+            } else if (key === 'type_next') {
+              if (!name) {
+                throw new SyntaxError(`expecting name, got '${token}' at ${this.pos}`);
+              }
+              if (!type) {
+                type = new Type;
+                type.name = name;
+              }
+              types.push(type);
+              type = null;
+              name = null;
+            } else if (key === 'fixed_collection_start' || key === 'collection_start') {
+              name = name || 'Array';
+              type = key === 'collection_start' ? new CollectionType : new FixedCollectionType;
+              type.name = name;
+              type.types = this.parse();
+            } else if (key === 'hash_collection_start') {
+              name = name || 'Hash';
+              type = new HashCollectionType;
+              type.name = name;
+              type.key_types = this.parse();
+              type.value_types = this.parse();
+            } else if (key === 'hash_collection_next' || key === 'hash_collection_end' ||
+                       key === 'fixed_collection_end' || key === 'collection_end' || key == 'parse_end') {
+              if (!name) {
+                throw new SyntaxError(`expecting name, got '${token}'`);
+              }
+              if (!type) {
+                type = new Type;
+                type.name = name;
+              }
+              types.push(type);
+              result = types;
+            }
+
+            throw BreakException;
+          });
+          throw new SyntaxError(`invalid character at ${this.input[0]}`);
+        } catch (e) {
+          if (e !== BreakException) throw e;
+        }
+      }
+      return result;
+    } catch (e) {
+      if (e instanceof SyntaxError) {
+        return e.message;
+      }
+      throw e;
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const submit = document.getElementById('types_submit');
+  const results = document.getElementById('types_results');
+  const input = document.getElementById('types_text');
+  if (!submit) return;
+
+  submit.addEventListener('click', () => {
+    const result = TypesParser.parse(input.value);
+    const html = result.map((v) => v.toString(false)).join(';<br>\n');
+    results.innerHTML = html;
+  });
+
+  input.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) submit.click();
+  });
+
+  document.querySelectorAll('#examples dd').forEach((el) => {
+    el.addEventListener('click', () => {
+      input.value = el.innerText;
+      submit.click();
+      document.location = '#see_results';
+    });
+  });
+});
